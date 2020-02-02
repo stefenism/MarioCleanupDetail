d7401017d9fa3302e4c4674c39fe82f33934927d
warreo <warcoding@gmail.com>
2020-02-02 12:51:11 -0600

    Started on Flag Cranking

--- Assets/scripts/interactables/Flag.cs
+++ Assets/scripts/interactables/Flag.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public class Flag : InteractableObject
 {
-    // Start is called before the first frame update
+    public float dropDistance = 5f;
+    Vector3 startPos;
+
+    private void Awake() {
+        base.Awake();
+
+    }
+
+    public override void Interacted(){
+        Vector3 currentPos = this.transform.GetChild(1).gameObject.transform.position;
+        if(dropDistance > 0f){
+            
+            dropDistance -= 1f;
+        }
+    }
 
 }
